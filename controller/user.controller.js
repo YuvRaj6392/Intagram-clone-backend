ea1e04af0acb576697a715e1037195f288fe687f
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-05-11 22:12:47 +0530

    added the login controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,7 @@
 const db=require('../models/index');
 const bcrypt = require("bcrypt");
+const jwt=require('jsonwebtoken');
+const secret="benzema123";
 const User=db.users;
 
 //controller for signup
@@ -13,14 +15,14 @@ exports.signup = async (req, res) => {
       {
         return res.status(400).json({
             success: false,
-            error: "Fill all the fields",
+            error: "Fill all the fields!",
           });
       }
       const user = await User.findOne({ email });
       if (user) {
         return res.status(400).json({
           success: false,
-          error: "User already exists",
+          error: "User already exists!",
         });
       }
   
@@ -32,7 +34,7 @@ exports.signup = async (req, res) => {
         password: hash,
         isLoggedIn: false,
       });
-  
+      
       return res.status(200).json({
         success: true,
         user: newUser,
@@ -43,8 +45,57 @@ exports.signup = async (req, res) => {
   
       return res.status(500).json({
         success: false,
-        error: "Internal server error",
+        error: "Internal server error!",
       });
     }
   };
+
+
+  //controller for singin
+  exports.signin=async (req,res)=>
+  {
+    try
+    {
+      const {email,password}=req.body;
+      if(!email || !password)
+      {
+        return res.status(400).json({
+          success:false,
+          message:'Fill all the fields!'
+        })
+      }
+      const user= await User.findOne({email:email});
+      if(user===null)
+      {
+        return res.status(400).json({
+          success:false,
+          message:'Please use correct credentials!'
+        })
+      }
+      const comparePassword=await  bcrypt.compareSync(password,user.password);
+      if(!comparePassword)
+      {
+        return res.status(400).json({
+          success:false,
+          message:'Please use correct credentials!'
+        })
+      }
+      const update={isLoggedIn:true};
+      await User.findOneAndUpdate({email:email},update,{
+        userFindAndModify:true
+      })
+      const data=user.id;
+      const jwtToken=jwt.sign(data,secret);
+      res.status(200).json({
+        success:true,
+        message:jwtToken
+      })
+
+    }catch(ex){
+      res.status(500).json({
+        success:false,
+        message:"Internal server error!"
+      })
+    }
+  }
   
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,5 +2,6 @@ const User=require('../controller/user.controller');
 module.exports=app=>{
     const router=require('express').Router();
     router.post('/signup',User.signup);
+    router.post('/signin',User.signin);
     app.use('/api',router);
 }
